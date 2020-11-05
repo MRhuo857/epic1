365da136c75d4314ed2d80510665a78043b81dce
mr.huo <455801665@qq.com>
2020-11-05 21:01:06 +0800

    按需加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {lazy, Suspense } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Loading from './component/Loading';
 
+const Home=lazy(()=>import('./pages/Home'))
+const About=lazy(()=>import('./pages/About'))
+const History=lazy(()=>import('./pages/History'))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Suspense fallback={Loading}>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/history">
+            <History />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
+
+      <Footer/>
+    </Router>
   );
 }
-
 export default App;
