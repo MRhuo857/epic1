8a49c8dd1505138a73441ea7fc34934d4c6b1885
mr.huo <455801665@qq.com>
2020-11-06 20:19:40 +0800

    引入logo

---
+++ src/pages/About.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function About() {
+  return <h2>关于</h2>;
+}
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Home() {
+  return <h2>首页</h2>;
+}
+export default Home
